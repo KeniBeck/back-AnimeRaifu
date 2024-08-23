de995a32439f5b27b339c75f0d5e72f054e239ce
KeniBeck <deyler_mena69222@elpoli.edu.co>
2024-08-23 18:39:20 -0500

    configuracion del ORM prisma en entorno MongoDB

---
+++ src/anime/anime.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAnimeDto } from './dto/create-anime.dto';
+import { UpdateAnimeDto } from './dto/update-anime.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class AnimeService {
+  constructor(private prisma: PrismaService) { }
+  create(createAnimeDto: CreateAnimeDto) {
+    return this.prisma.anime.create({
+      data: {
+        ...createAnimeDto
+      }
+    });
+  }
+
+  findAll() {
+    return `This action returns all anime`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} anime`;
+  }
+
+  update(id: number, updateAnimeDto: UpdateAnimeDto) {
+    return `This action updates a #${id} anime`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} anime`;
+  }
+}
